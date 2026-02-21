f03b9c5e8a4c3b3f9cb9fa395af8b4b1060f74f7
DIANDRADZIL <diandra04dzilm@gmail.com>
2026-02-21 13:17:18 +0800

    tugas kelompok 5

--- lembar.py
+++ lembar.py
@@ -1,3 +1,5 @@
+#Conditional Statements
+
 #penggunaan random
 import random
 
@@ -76,21 +78,123 @@ elif x == 0:
 else:
     print("x adalah bilangan negatif")
 
-#Penggunaan operator logika dalam if
-x = int(input("Masukkan nilai x: "))
-if x > 0 and x % 2 == 0:
-    print("x adalah bilangan positif dan genap")
-elif x > 0 and x % 2 != 0:
-    print("x adalah bilangan positif dan ganjil")
+#Penggunaan nested if kasus lain
+umur = int(input("Untuk caleg, silahkan masukkan umur: "))
+punya_ktp = True
+
+if umur >= 17:
+    if punya_ktp:
+        print("Boleh memilih")
+    else:
+        print("Harus punya KTP dulu")
 else:
-    print("x bukan bilangan positif")
+    print("Belum cukup umur")
+
+#Penggunaan operator logika dalam if
+y = int(input("Masukkan nilai Y: "))
+if y > 0 and y % 2 == 0:
+    print("Y adalah bilangan positif dan genap")
+elif y > 0 and y % 2 != 0:
+    print("Y adalah bilangan positif dan ganjil")
+else:
+    print("Y bukan bilangan positif")
 
 #Penggunaan Ternary Operator
-x = int(input("Masukkan nilai x: "))
-status = "Positif" if x > 0 else "Negatif atau Nol"
-print(f"x adalah bilangan {status}")
+z = int(input("Masukkan nilai Z: "))
+status = "Positif" if z > 0 else "Negatif atau Nol"
+print(f"Z adalah bilangan {status}")
 
 #variable yang dapat digunakan dalam if, elif
 is_active = True
 if is_active:
     print("Selamat akun aktif")
+
+#Iterative Statements
+
+#Perbedaan for dan while
+# Kasus for (jumlah sudah pasti 5 kali)
+for i in range(7):
+    print("(For)Perulangan ke-", i)
+
+# Kasus while
+i = 0
+while i < 9:
+    print("(While)Perulangan ke-", i)
+    i += 1
+
+#Break
+for i in range(35):
+    if i == 25:
+        break
+    print(i)
+
+#Continue
+for i in range(15):
+    if i == 10:
+        continue
+    print(i)
+
+#Else di loop
+for i in range(7):
+    print(i)
+else:
+    print("Loop selesai, tidak ada break")
+
+#Nested di loop
+for i in range(5):
+    for j in range(5):
+        print(i, j)
+
+#Aplikasi literasi dalam algoritma
+#Membuat pola bintang
+for i in range(10):
+    for j in range(i+1):
+        print("*", end="")
+    print()
+
+#Pembuatan Piramid
+tinggi = int(input("Masukkan tinggi piramida yang anda inginkan: "))
+
+for i in range(tinggi):
+    print(" " * (tinggi - i - 1) + "* " * (i + 1))
+
+#Menghitung Jumlah Total
+angka = [14, 29, 31, 65]
+total = 0
+for nilai in angka:
+    total += nilai
+
+print("(Penghitungan Jumlah Total) Total:", total)
+
+#Faktorial
+n = int(input("Masukkan nilai yang ingin di faktorkan: "))
+faktorial = 1
+
+for i in range(1, n+1):
+    faktorial *= i
+
+print("Faktorial:", faktorial)
+
+#Validasi input dengan while
+nilai = -1
+while nilai < 0:
+    nilai = int(input("Masukkan angka positif: "))
+
+print("Input diterima")
+
+#Transfer Statment
+nilai = int(input("Masukkan Nilai Rapor: "))
+if nilai > 75:
+    pass
+else:
+    print("Anda Mengulang")
+
+#Return Statment
+hasil = 0
+
+def hitung(a, b):
+    global hasil
+    hasil = a + b
+
+hitung(27, 9)
+print(hasil)
